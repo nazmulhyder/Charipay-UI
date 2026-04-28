c3dfdb8e09bb55374a45293c1515adeee3fe81fd
nazmulhyder <nhyder.cse131.uiu@gmail.com>
2026-04-28 07:52:31 +0100

    donor dashboard scroll on top problem has been fixed

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
 import { routes } from './app.routes';
@@ -10,7 +10,12 @@ export const appConfig: ApplicationConfig = {
   providers:[
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes,
+      withInMemoryScrolling({
+    scrollPositionRestoration: 'enabled',
+    anchorScrolling: 'enabled'
+  })
+    ),
     provideHttpClient(
       withFetch(),
       withInterceptorsFromDi()
